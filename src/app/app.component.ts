04d264d80b8d08074942f7b6c8030ebd0a9afbc3
testuser <testuser@example.com>
2020-12-09 20:53:20 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ccp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'credit-card';
+}
